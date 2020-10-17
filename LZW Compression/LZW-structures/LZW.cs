9a7114bda8911fd447b03044ba5258ffb82e3481
0112358132134-2026 <55027767+0112358132134-2026@users.noreply.github.com>
2020-10-16 20:37:50 -0600

    Compresión LZW
    Casi lista!

--- LZW Compression/LZW-structures/LZW.cs
+++ LZW Compression/LZW-structures/LZW.cs
@@ -78,24 +78,171 @@ namespace LZW_structures
             } 
         }
 
-        public byte[] CompressedText()
+        public byte[] CompressedText(int maxValue, string originalName)
         {
-            return null;
+            string binaryMajor = ConvertDecimalToBinary(maxValue);
+            StringBuilder binaries = new StringBuilder();
+            for (int i = 0; i < textInNumbers.Count; i++)
+            {
+                StringBuilder binary = new StringBuilder();
+                binary.Append(ConvertDecimalToBinary(textInNumbers[i]));
+                if (binary.Length < binaryMajor.Length)
+                {
+                    for (int j = binary.Length; j < binaryMajor.Length; j++)
+                    {
+                        binary.Insert(0,"0");
+                    }                    
+                }
+                binaries.Append(binary.ToString());
+            }
+            //Con el string "binaries" individuales de cada número, dividimos de 8 en 8 bits: 
+            List<string> bytesToCompressedText = SeparateBytes(binaries.ToString());
+            //Convertimos los bytes (en binario) a decimales y los agregamos a otra lista:
+            List<int> bytesCompressedTex = new List<int>();
+            for (int i = 0; i < bytesToCompressedText.Count; i++)
+            {
+                bytesCompressedTex.Add(ConvertBinaryToDecimal(bytesToCompressedText[i]));
+            }
+            //Obtenemos la tabla original en bytes:
+            List<int> originalTableToBytes = OriginalTableToBytes();
+            //Agregamos todos los bytes al arreglo resultante:
+            List<int> auxResult = new List<int>();
+
+            List<byte> oName = BytesToOriginalName(originalName);
+            for (int i = 0; i < oName.Count; i++)
+            {
+                auxResult.Add((int)oName[i]);
+            }
+
+            auxResult.Add(binaryMajor.Length);
+            auxResult.Add(posOriginalData);
+            for (int i = 0; i < originalTableToBytes.Count; i++)
+            {
+                auxResult.Add(originalTableToBytes[i]);
+            }
+            for (int i = 0; i < bytesCompressedTex.Count; i++)
+            {
+                auxResult.Add(bytesCompressedTex[i]);
+            }           
+            byte[] result = new byte[oName.Count + 2 + originalTableToBytes.Count + bytesCompressedTex.Count];
+            for (int i = 0; i < auxResult.Count; i++)
+            {
+                result[i] = (byte)auxResult[i];
+            }
+            return result;
         }
+
+        public List<int> OriginalTableToBytes()
+        {
+            List<string> characters = new List<string>();
+            List<int> result = new List<int>();
+            int counter = 0;
+            foreach (KeyValuePair<string, int> symbol in symbols)
+            {
+                while (counter < posOriginalData)
+                {
+                    characters.Add(symbol.Key);
+                    counter++;
+                }
+            }
+            //
+            for (int i = 0; i < characters.Count; i++)
+            {
+                char[] oka = characters[i].ToCharArray();
+                result.Add((byte)oka[0]);
+            }
+            return result;
+        }
+
+        public List<string> SeparateBytes(string largeBinary)
+        {
+            StringBuilder copy = new StringBuilder();
+            copy.Append(largeBinary);
+            List<string> result = new List<string>();
+            bool OK = false;
+            while (!OK)
+            {
+                if (copy.Length >= 8)
+                {
+                    result.Add(copy.ToString(0, 8));
+                    copy.Remove(0, 8);
+                }
+                else
+                {
+                    if (copy.Length > 0)
+                    {
+                        for (int i = copy.Length; i < 8; i++)
+                        {
+                            copy.Append("0");
+                        }
+                        result.Add(copy.ToString());
+                    }                   
+                    OK = true;
+                }
+            }
+            return result;
+        }
+
+        public List<byte> BytesToOriginalName(string originalName)
+        {
+            List<byte> listAux = new List<byte>();
+            for (int i = 0; i < originalName.Length; i++)
+            {
+                char ok = (char)originalName[i];
+                listAux.Add((byte)ok);
+            }
+            listAux.Add(10);
+            return listAux;
+        }
+
 
         public byte[] Compression(string path, string originalName)
         {
             OriginalTable(path);
             TextInNumbers(path);
             int maxNumber = textInNumbers.Max();
-
-            return null;
+            byte[] compressedText = CompressedText(maxNumber, originalName);
+            return compressedText;
         }
-
 
         public List<char> Decompression(List<byte> bytes)
         {
             return null;
         }
+
+        #region "Auxiliaries"
+        public int ConvertBinaryToDecimal(string binary)
+        {
+            int exponent = binary.Length - 1;
+            int decimalNumber = 0;
+
+            for (int i = 0; i < binary.Length; i++)
+            {
+                if (int.Parse(binary.Substring(i, 1)) == 1)
+                {
+                    decimalNumber += int.Parse(System.Math.Pow(2, double.Parse(exponent.ToString())).ToString());
+                }
+                exponent--;
+            }
+            return decimalNumber;
+        }
+        public string ConvertDecimalToBinary(int number)
+        {
+            string result = "";
+            while (number > 0)
+            {
+                if (number % 2 == 0)
+                {
+                    result = "0" + result;
+                }
+                else
+                {
+                    result = "1" + result;
+                }
+                number = (int)(number / 2);
+            }
+            return result;
+        }
+        #endregion
     }
 }
